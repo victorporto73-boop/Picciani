67ccb2c29cacbfc1bf50f1dc83f8cc5d229a0c9c
victorporto73-boop <victorporto73@gmail.com>
2026-08-11 13:51:10 -0300

    Bump cache do service worker para v2

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cadastro-v1';
+const CACHE = 'cadastro-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (e) => {
